c416fbb2a3cd3d66af2bfbe0373ea289cabe8b48
Hamid Shahid <hamidshahid599@gmail.com>
2025-01-10 23:28:51 +0500

    added calculate_chunk_id and main functions

--- populate_database.py
+++ populate_database.py
@@ -53,6 +53,57 @@ def add_to_chroma(chunks: list[Document]):
         print("documents added successfully")
     else:
         print("✅ No new documents to add")
+        
+def calculate_chunk_ids(chunks):
 
+    # This will create IDs like "data/monopoly.pdf:6:2"
+    # Page Source : Page Number : Chunk Index
+
+    last_page_id = None
+    current_chunk_index = 0
+
+    for chunk in chunks:
+        source = chunk.metadata.get("source")
+        page = chunk.metadata.get("page")
+        current_page_id = f"{source}:{page}"
+
+        # If the page ID is the same as the last one, increment the index.
+        if current_page_id == last_page_id:
+            current_chunk_index += 1
+        else:
+            current_chunk_index = 0
+
+        # Calculate the chunk ID.
+        chunk_id = f"{current_page_id}:{current_chunk_index}"
+        last_page_id = current_page_id
+
+        # Add it to the page meta-data.
+        chunk.metadata["id"] = chunk_id
+
+    return chunks
+
+def main():
+
+    # Check if the database should be cleared (using the --clear flag).
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reset", action="store_true", help="Reset the database.")
+    args = parser.parse_args()
+    if args.reset:
+        print("✨ Clearing Database")
+        clear_database()
+
+    # Create (or update) the data store.
+    documents = load_documents()
+    chunks = split_documents(documents)
+    # print(chunks[2])
+    add_to_chroma(chunks)
+
+def clear_database():
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+
+if _name_ == "_main_":
+    main()
 
 

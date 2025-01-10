8eeb097fe204ea0d49132769193cdbf2a8457eba
Hamid Shahid <hamidshahid599@gmail.com>
2025-01-10 23:23:52 +0500

    function to populate chroma database

---
+++ populate_database.py
@@ -0,0 +1,58 @@
+import argparse
+import os
+import shutil
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+from get_embedding_function import get_embedding_function
+# from langchain.vectorstores.chroma import Chroma
+from langchain_chroma import Chroma
+
+CHROMA_PATH = r"F:\FYP\Current\o-level-physics-5054-20241117T145438Z-001"
+
+def load_documents():
+    document_loader = PyPDFDirectoryLoader('./physics_qp')
+    return document_loader.load()
+# documents = load_documents()
+# print(documents[3])
+
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=800,
+        chunk_overlap=80,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    return text_splitter.split_documents(documents)
+
+def add_to_chroma(chunks: list[Document]):
+    # Load the existing database.
+    db = Chroma(
+        persist_directory=CHROMA_PATH, embedding_function=get_embedding_function()
+    )
+
+    # Calculate Page IDs.
+    chunks_with_ids = calculate_chunk_ids(chunks)
+
+    # Add or Update the documents.
+    existing_items = db.get(include=[])  # IDs are always included by default
+    existing_ids = set(existing_items["ids"])
+    print(f"Number of existing documents in DB: {len(existing_ids)}")
+
+    # Only add documents that don't exist in the DB.
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata["id"] not in existing_ids:
+            new_chunks.append(chunk)
+
+    if len(new_chunks):
+        print(f"👉 Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata["id"] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+        # db.persist()
+        print("documents added successfully")
+    else:
+        print("✅ No new documents to add")
+
+
+
